3c1915ed359107a35b38c34b66b59db00b2f1765
Khizarshah01 <khizar.shah010@gmail.com>
2025-01-29 00:49:11 +0530

    Fix: add cors

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import Flask-CORS
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
 import joblib
 
-app = Flask(__name__,template_folder="index.html")
+app = Flask(__name__, template_folder="index.html")
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/carrer', methods=['POST'])
 def carrer():
     user_data = request.json
     user_df = pd.DataFrame([user_data])
+   # print(user_df)
 
     logistic_model = joblib.load("logistic_model.pkl")
     label_encoder = joblib.load("label_encoder.pkl")
@@ -19,4 +22,4 @@ def carrer():
     return jsonify({"recommendedCareer": career})
 
 if __name__ == '__main__':
-    app.run(debug=True,port=3000)
+    app.run(debug=True, port=3300)
